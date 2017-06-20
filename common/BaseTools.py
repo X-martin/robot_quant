94e10098529e346ef2a5252e3b63311079d53558
jessen <312157238@qq.com>
2017-06-20 17:26:09 +0800

    ad mysql connection

--- common/BaseTools.py
+++ common/BaseTools.py
@@ -2,12 +2,19 @@
 #coding=utf-8
 
 '''
-工具
+工具类
 
 '''
 
 import datetime
+import MySQLdb
 
+'''
+获取mysql的链接
+'''
+def getConnection():
+    conn = MySQLdb.connect(host='192.168.2.31', user='root', passwd='1qaz@WSX+!', db='strategy', port=3306)
+    return conn
 
 def dateRange(beginDate, endDate):
     dates = []
@@ -22,3 +29,9 @@ def dateRange(beginDate, endDate):
 
 if __name__ == '__main__':
     print dateRange("2016-01-01", "2016-02-01")
+    conn = getConnection()
+    c = conn.cursor()
+    sql = "select * from r_strategy"
+    c.execute(sql)
+    alldata = c.fetchall()
+    print alldata
