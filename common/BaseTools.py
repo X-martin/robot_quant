5b9c0640ec9e1630990fef81f81b5e5d568f2d8f
jessen <312157238@qq.com>
2017-06-29 14:55:19 +0800

    增加工具函数

--- common/BaseTools.py
+++ common/BaseTools.py
@@ -27,6 +27,33 @@ def dateRange(beginDate, endDate):
         date = dt.strftime("%Y-%m-%d")
     return dates
 
+'''
+获取最近的季度日期
+'''
+def getLastestQuarterlistByDate(d):
+    year = d.year
+    month = d.month
+    day = d.day
+    # print year, month, day
+    quarter=[]
+    if month <= 12 and month > 9:
+        quarter.append((year, 3))
+        quarter.append((year, 2))
+        quarter.append((year, 1))
+    elif month <= 9 and month > 6:
+        quarter.append((year, 2))
+        quarter.append((year, 1))
+        quarter.append((year-1, 4))
+    elif month <= 6 and month > 3:
+        quarter.append((year, 1))
+        quarter.append((year-1, 4))
+        quarter.append((year-2, 3))
+    elif month <= 3 and month > 0:
+        quarter.append((year-1, 4))
+        quarter.append((year-1, 3))
+        quarter.append((year-1, 2))
+    return quarter
+
 if __name__ == '__main__':
     print dateRange("2016-01-01", "2016-02-01")
     conn = getConnection()
@@ -35,3 +62,5 @@ if __name__ == '__main__':
     c.execute(sql)
     alldata = c.fetchall()
     print alldata
+
+    print getLastestQuarterlistByDate(datetime.datetime.now())
