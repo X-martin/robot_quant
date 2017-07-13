98599418d5a2d782713a462cb3f8f91a02d149b6
jessen <312157238@qq.com>
2017-07-13 15:08:38 +0800

    增加全局变量

--- common/Constants.py
+++ common/Constants.py
@@ -21,3 +21,15 @@ positionList = {}
 # 自定义参数词典
 diyParams = {}
 
+periodType='D'
+changePeriod=20
+
+currentPeriod = 1
+
+# 设置手续费
+# 买入时万分之三
+buyCost=0.0003
+# 卖出时万分之三加千分之一印花税
+sellCost=0.0013
+# 每笔交易最低扣5块钱
+minCost=5
