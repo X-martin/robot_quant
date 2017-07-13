f75ed0853b731891fcaf25ba7661bf3e831b3da1
stephenncd <stephenncd@gmail.com>
2017-07-12 21:25:59 -0400

    update factor class, completed apply_method function

--- frame/factor.py
+++ frame/factor.py
@@ -3,47 +3,53 @@ import pandas as pd
 
 class Factor(object):
 
-    def __init__(self, name, method):
+    def __init__(self, name):
         self.name = name
-        self.method = method
+        self.method = None
+        self.value = None
         self.date = None
-        self.stocks = pd.DataFrame(columns=["stock_id", "value"])
+        self.stocks = pd.DataFrame(columns=["stock_id", "factor_value"])
 
-    def get_values(self):
+    def get_factor_values(self):
         # pass args self.date, self.name, self.stocks
         # call Tushare Api
         # rewrite self.values
         pass
 
     def apply_method(self):
-        method = self.method[0]
-        quant = self.method[1]
-        if method == "asc top":
-            pass
-        elif method == "asc bottom":
-            pass
-        elif method == "desc top":
-            pass
-        elif method == "desc bottom":
-            pass
+        if method in ["asc top", "asc bottom", "desc top", "desc bottom"]:
+            if self.value > 0:
+                if self.value < 1:
+                    self.value = int(self.value * len(self.stocks))
+                self.value = min([self.value, len(self.stocks)])
+                if method in ["asc top", "desc bottom"]:
+                    self.stocks = self.stocks.sort_values(["factor_value"], ascending=True)
+                else:
+                    self.stocks = self.stocks.sort_values(["factor_value"], ascending=False)
+                self.stocks = self.stocks.iloc[:self.value, :]
         elif method == "is":
-            pass
+            self.stocks = self.stocks.loc[self.stocks["factor_value"] == self.value]
         elif method == "not":
-            pass
+            self.stocks = self.stocks.loc[self.stocks["factor_value"] != self.value]
         elif method == "in":
-            pass
+            self.stocks = self.stocks.loc[self.stocks["factor_value"].apply(
+                lambda x: x in self.value)]
         elif method == "not in":
-            pass
+            self.stocks = self.stocks.loc[self.stocks["factor_value"].apply(
+                lambda x: x not in self.value)]
         elif method == "greater":
-            pass
+            self.stocks = self.stocks.loc[self.stocks["factor_value"] > self.value]
         elif method == "less":
-            pass
+            self.stocks = self.stocks.loc[self.stocks["factor_value"] < self.value]
         elif method == "between":
-            pass
+            self.stocks = self.stocks.loc[self.stocks["factor_value"] > self.value[0]]
+            self.stocks = self.stocks.loc[self.stocks["factor_value"] < self.value[1]]
 
-    def filter(self, stocks, date):
+    def filter(self, stocks, date, method, value):
         self.stocks["stock_id"] = stocks
         self.date = date
+        self.method = method
+        self.value = value
         self.get_values()
         self.apply_method()
         return list(self.stocks["stock_id"])
