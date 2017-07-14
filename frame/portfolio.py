ac373c823e60a952a0866f797cc78d471677385a
stephenncd <stephenncd@gmail.com>
2017-07-13 22:00:52 -0400

    create class portfolio

---
+++ frame/portfolio.py
@@ -0,0 +1,19 @@
+#----------------------------------
+
+
+class Portfolio(object):
+
+    def __init__(self):
+        pass
+
+    def trigger(self):
+        pass
+
+    def trade(self):
+        pass
+
+    def backtesting(self):
+        pass
+
+    def push2db(self):
+        pass
