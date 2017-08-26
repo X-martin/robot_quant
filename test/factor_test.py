1bbdf9cdc5b6bb90a5dea11e5e7bbd38d9e9cbf5
stephenncd <stephenncd@gmail.com>
2017-08-25 21:50:18 -0400

    pre version code passed test

--- test/factor_test.py
+++ test/factor_test.py
@@ -15,13 +15,13 @@ class FactorT(object):
         if self.method is None:
             time_list = [date]
             df_temp = dbst.get_base_factor_val(self.name, time_list, stock_list)
-        elif self.method[0] == 'MA':
+        elif self.method[0] == 'daily_avg':
             dt = timedelta(days=self.method[1])
             time_list = [date - dt, date]
             df_temp = dbst.get_base_factor_val(self.name, time_list, stock_list)
             df_temp = df_temp.groupby('stock_id').mean()
             df_temp = df_temp.reset_index()
-        elif self.method[0] == 'Lag':
+        elif self.method[0] == 'lag':
             dt = timedelta(days=self.method[1])
             time_list = [date - dt]
             df_temp = dbst.get_base_factor_val(self.name, time_list, stock_list)
@@ -46,20 +46,84 @@ class FilterT(object):
 
     def single_filter(self, stock_list, date):
         df = self.factors[0].get_val(stock_list, date)
-        return df['stock_id']
+        if self.method[0] == 'asc':
+            df = df.sort_values(["val"], ascending=True)
+            nn = self.method[1]
+            if nn < 1:
+                nn = int(len(df) * nn)
+            df = df.iloc[:nn, :]
+        if self.method[0] == 'desc':
+            df = df.sort_values(["val"], ascending=False)
+            nn = self.method[1]
+            if nn < 1:
+                nn = int(len(df) * nn)
+            df = df.iloc[:nn, :]
+        return list(df['stock_id'])
 
     def double_filter(self, stock_list, date):
         df1 = self.factors[0].get_val(stock_list, date)
-        df2 = self.factors[0].get_val(stock_list, date)
+        df2 = self.factors[1].get_val(stock_list, date)
+        if self.method[0] == 'cross_up':
+            dt = timedelta(days=1)
+            df1_0 = self.factors[0].get_val(stock_list, date - dt)
+            df2_0 = self.factors[1].get_val(stock_list, date - dt)
+            id = (df1_0['val'] < df2_0['val']) & (df1['val'] > df2['val'])
+            df1 = df1[id]
+        elif self.method[0] == 'cross_down':
+            dt = timedelta(days=1)
+            df1_0 = self.factors[0].get_val(stock_list, date - dt)
+            df2_0 = self.factors[1].get_val(stock_list, date - dt)
+            id = (df1_0['val'] > df2_0['val']) & (df1['val'] < df2['val'])
+            df1 = df1[id]
+        elif self.method[0] == 'greater_than':
+            df1 = df1[df1['val'] > df2['val']]
+        elif self.method[0] == 'less_than':
+            df1 = df1[df1['val'] < df2['val']]
+        return list(df1['stock_id'])
 
 if __name__=='__main__':
-    name = 'close'
-    ma5 = FactorT(name, ['MA', 5])
-    ma10 = FactorT(name, ['MA', 10])
+    d_factors = {}
+    d_filters = {}
 
-    t1 = datetime.strptime('2017/08/23', '%Y/%m/%d')
+    # ----user's input-------
+    t1 = datetime.strptime('2017/08/03', '%Y/%m/%d')
     stock_list = ['GOOG', 'C', 'IBM', 'F', 'AAPL', 'KO', 'LEN', 'MO', 'AMZN', 'FLS', 'APD']
-    dfma5 = ma5.get_val(stock_list, t1)
-    dfma10 = ma10.get_val(stock_list, t1)
-    print dfma5
-    print dfma10
+
+    input_factors = ['ma5 is close 5 daily_avg',
+                     'ma10 is close 10 daily_avg']
+
+    input_filters = ['golden_cross is ma5 cross_up ma10',
+                     'ma5gma10 is ma5 greater_than ma10']
+
+    # ----create factors------
+    for l in input_factors:
+        l = l.split(' is ')
+        factor_name = l[0]
+        l_right = l[1].split()
+        name = l_right[0]
+        method = [l_right[2], float(l_right[1])]
+        d_factors[factor_name] = FactorT(name, method)
+
+    # ----create filters------
+    for l in input_filters:
+        l = l.split(' is ')
+        filter_name = l[0]
+        l_right = l[1].split()
+        factors = []
+        w_float = 0
+        for w in l_right:
+            try:
+                w_float = float(w)
+                l_right.remove(w)
+            except:
+                if w in d_factors.keys():
+                    factors += [d_factors[w]]
+                    l_right.remove(w)
+        method = [l_right[0], w_float]
+        d_filters[filter_name] = FilterT(factors, method)
+
+    # ----get daily stock list-----
+    for key in d_filters.keys():
+        stocks = d_filters[key].filter(stock_list, t1)
+        print key
+        print stocks
