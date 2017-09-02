48828ec9454c83a5bc7dfdc3fb97abbd4f05b268
stephenncd <stephenncd@gmail.com>
2017-09-02 09:40:53 -0400

    factor_method

--- test/resolve_test.py
+++ test/resolve_test.py
@@ -21,21 +21,23 @@ def resolve_condition(line):
     line = line.replace(' ', '')
     l = line.split('=')
     condition_name = l[0]
-    l = l[1][-1].split('(')
+    l = l[1][:-1].split('(')
     method = [l[0]]
     l = l[1].split(',')
     method += [l]
     return condition_name, method
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     input_factors = 'MA5 = MA(close,5)\nMA10 = MA(close, 10)'
     input_condition = 'gx = CROSS(MA5, MA10)'
 
     input_factors_lines = input_factors.split('\n')
     for line in input_factors_lines:
-        print resolve_factor(line)
+        if len(line)>1:
+            print resolve_factor(line)
 
     input_condition_lines = input_condition.split('\n')
     for line in input_condition_lines:
-        print resolve_condition(line)
+        if len(line)>1:
+            print resolve_condition(line)
