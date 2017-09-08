e1f8f95adf7cfbe0bf0294c83596e15062ecf47e
jessen <312157238@qq.com>
2017-09-08 13:54:17 +0800

    修改回测函数

--- common/StrategyTools.py
+++ common/StrategyTools.py
@@ -37,69 +37,8 @@ import traceback
        否则，更新仓位，减去仓位数量,修改价格
        （仓位价格*仓位数量+订单价格*订单数量）/（仓位数量+订单数量）
 
-
-    不是每天都会有订单，每天都要有仓位
-
-    买入金额:
-
-    卖出金额:
-    当前价
-    仓位均价
-    仓位数量
-    卖出数量
-
-    如果当前价大于仓位均价,上涨
-
-    87.88
-
-    69.26-----------------64.63
-
-    1000
-
-    200
-
-    总资产:774464.23
-    可用资金:53189.53--------------70746
-
-    卖出价为87.88--当前价
-
-    卖出后重新计算成本价
-
-    总资产:774316.55
-    可用资金:70746.95
-
-    当前市值:70096---52578
-
-    平均成本:64.63----57.01
-
-    现价:87.62
-
-    800股
-
-    成本:10元 1000
-    涨到12元
-
-    卖出200,成交价12
-
-    账户多出12*200,成本
-
-    12
-
-    如果不降低成本，卖出时按当前价
-
-
-
-    处理完订单，通过前一天
-    记录一条每日可用资产，总资产--------------仓位信息
-    1,000000,tradedate,1000000,1000000
-
-
-    查询最新的仓位日期
-    通过日期查询当前仓位
-
-
 '''
-def order(orderList, conn):
+def order(orderList, stockDict, conn):
     orderListNew = []
     try:
         if len(orderList) == 0:
@@ -142,19 +81,19 @@ def order(orderList, conn):
             if order.volume > 0: # 如果订单中的量大于0，该订单为买入
                 tradeMoney = tradeMoney - order.price * order.volume
                 if order.stockcode not in positionStocklist:
-                    v = (order.strategyId, order.stockcode, order.tradedate, order.price, order.volume)
+                    v = (order.strategyId, order.stockcode, order.tradedate, order.price, order.price, order.volume)
                     positionInsertList.append(v)
                 else:
                     p = positionMap[key]
                     price = (p[3]*p[4]+order.price*order.volume) / (p[4]+order.volume)
-                    u = (order.strategyId, order.stockcode, order.tradedate, price, order.volume)
+                    u = (order.strategyId, order.stockcode, order.tradedate, price, price, order.volume)
                     positionUpdateList.append(u)
             elif order.volume < 0: # 如果订单中的量小于0，该订单为卖出
                 if order.stockcode not in positionStocklist:
                     continue
                 elif p.volume <= -1*order.volume:
                     p = positionMap[key]
-                    d = (order.strategyId, order.stockcode, order.tradedate, p.price, p.volume)
+                    d = (order.strategyId, order.stockcode, order.tradedate, p.price, p.price, p.volume)
                     positionDeleteList.append(d)
                 else:
                     p = positionMap[key]
@@ -164,12 +103,12 @@ def order(orderList, conn):
                     # 10元钱买入1000股，涨价20元钱，卖出500
                     # （（10*1000）-20*（1000-500）
                     # price = (p[3]*p[4]+order.price*order.volume) / (p[4]+order.volume)
-                    v = (order.strategyId, order.stockcode, order.tradedate, order.price, order.volume)
+                    v = (order.strategyId, order.stockcode, order.tradedate, order.price, order.price, order.volume)
                     positionUpdateList.append(v)
                 tradeMoney = tradeMoney - order.price * order.volume
             elif order.volume == 0: # 如果订单中的量等于0，该订单为卖出全部
                 p = positionMap[key]
-                d = (order.strategyId, order.stockcode, order.tradedate, p.price, p.volume)
+                d = (order.strategyId, order.stockcode, order.tradedate, p.price, p.price, p.volume)
                 positionDeleteList.append(d)
                 # 如果订单的数量为0，全仓卖出，使用仓位数量，加钱
                 tradeMoney = tradeMoney + order.price * p.volume
@@ -188,6 +127,11 @@ def order(orderList, conn):
         for p in positionDeleteList:
             key = p[0]+'_'+str(p[2])
             del positionMap[key]
+
+        # 更新仓位中的股票实时价格
+        for k, v in positionMap.items():
+            v[3] = stockDict[v[1]]
+            positionMap[k] = v
 
         positionList = positionMap.values()
 
@@ -231,44 +175,8 @@ def getLastAccountDate(strategyId, conn):
 def savePosition(positionList, conn):
     try:
         cur = conn.cursor()
-        '''
-        # 查询最大日期
-        maxTradeDateSql = 'select max(tradedate) from r_position where strategy_id='+str(strategyId)
-        cur.execute(maxTradeDateSql)
-        maxTradeDate = cur.fetchone()
-        maxTradeDateStr = datetime.strftime(maxTradeDate[0], '%Y-%m-%d')
-
-        positionListSql = 'select * from r_position where strategy_id=%s and tradedate=DATE_FORMAT(\'%s\', \'%Y-%m-%d\')'
-        cur.execute(positionListSql, (strategyId, maxTradeDateStr))
-        positionList = cur.fetchall()
-        print positionList
-        positionMap = {}
-        for p in positionList:
-            positionMap[p[1]] = p
-
-        positionList = []
-        delPositionIds=[]
-        for order in orderList:
-            p = StockPosition.StockPosition(order.strategyId, order.stockcode, order.tradedate, order.price, order.volume)
-            v = (order.strategyId, order.stockcode, order.tradedate, order.price, order.volume)
-            positionList.append(v)
-            p = positionMap[order.stockcode]
-            if order.volume>0:
-                order.volume=p[4]+order.volume
-            elif order.volume==0:
-                # 清空仓位
-                pass
-            elif order.volume<0:
-                if p.volume>abs(order.volume):
-                    order.volume=p.volume+order.volume
-                elif p.volume<=abs(order.volume):
-                    # 清空仓位
-                    pass
-        '''
-
-
         cur.executemany(
-            'insert into r_position(strategy_id, stockcode, tradedate, price, volume) values(%s, %s, %s, %s, %s)',
+            'insert into r_position(strategy_id, stockcode, tradedate, current_price, price, volume) values(%s, %s, %s, %s, %s, %s)',
             positionList)
 
         conn.commit()
@@ -280,10 +188,10 @@ def savePosition(positionList, conn):
 '''
 def saveAccount(strategyId, tradedate, accountMoney, conn):
     cur = conn.cursor()
-    p = (strategyId, '000000', tradedate, accountMoney, accountMoney)
+    p = (strategyId, '000000', tradedate, accountMoney, accountMoney, accountMoney)
     positionList = [p]
     cur.executemany(
-        'insert into r_position(strategy_id, stockcode, tradedate, price, volume) values(%s, %s, %s, %s, %s)',
+        'insert into r_position(strategy_id, stockcode, tradedate, current_price, price, volume) values(%s, %s, %s, %s, %s, %s)',
         positionList)
 
 
