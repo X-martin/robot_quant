6412d312fe248e67ab155c3d37b9ab0f20ff4451
jessen <312157238@qq.com>
2017-09-08 14:02:18 +0800

    修改回测函数

--- common/StockPosition.py
+++ common/StockPosition.py
@@ -9,9 +9,10 @@
 
 class StockPosition(object):
 
-    def __init__(self, strategyId, stockcode, tradedate, price, volume):
+    def __init__(self, strategyId, stockcode, tradedate, current_price, price, volume):
         self.strategyId = strategyId
         self.stockcode = stockcode
         self.tradedate = tradedate
+        self.current_price = current_price
         self.price = price
         self.volume = volume
