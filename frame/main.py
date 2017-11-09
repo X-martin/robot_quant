e79e505ac9dd1fb9c749779316f6de595787518c
jessen <312157238@qq.com>
2017-11-09 14:40:37 +0800

    修改

--- frame/main.py
+++ frame/main.py
@@ -8,9 +8,6 @@
 
 import common.Constants as c
 import common.BaseTools as bt
-import shutil
-
-import user.strategy_diy as sd
 
 from jinja2 import Template
 from jinja2 import Environment, PackageLoader
@@ -43,21 +40,24 @@ class loader(object):
 '''
 def run():
     # 初始化
-    sd.init()
+    my.init()
     conn = bt.getConnection()
+    print c.startDateStr
+    print c.endDateStr
     df = bt.getTradeDay(conn, start_date_str=c.startDateStr, end_date_str=c.endDateStr, type=1)
+    print df
     # 按日期执行策略
-    datelist = bt.dateRange(c.startDateStr, c.endDateStr)
     datelist = df['tradedate'].tolist()
+
 
     load = loader()
     m = load.load("my.py")
     func = m["handle_data"]
 
-    for d in range(len(datelist)):
+    for d in datelist:
         # print d
         #sd.handle_data(datelist[d])
-        func(datelist[d])
+        func(d)
 
 def generateCode():
     stocklistStr = ''
@@ -87,47 +87,15 @@ def generateCode():
         if i != len(stocktype_list):
             stocklistStr += "+"
     print stocklistStr
-
+    buylist = ['gx', 'asc5']
+    selllist = ['gx', 'asc5']
     # 判断是否在这个时间段,如果是，调用查询股票池方法
     # stocklist1 = tbd.get_stocklist_by_type('', '000300.SH')
 
-    # 生成买入股票代码
-    buylistStr = ''
-    for i in range(len(buycondition_list)):
-        condition = buycondition_list[i]
-        buylistStr = buylistStr + '    # condition-'+str(i)+'\n'
-        if condition[0] == 'fin':
-            buylistStr = buylistStr + '    df = t.get_factor_data_by_stocklist(trade_date_str, stocklist, \''+condition[1]+'\', 0)\n'
-            if condition[2] == 'asc':
-                buylistStr = buylistStr + '    df = df.sort_values(by=\'fv\', ascending=True)\n'
-            elif condition[2] == 'desc':
-                buylistStr = buylistStr + '    df = df.sort_values(by=\'fv\', ascending=False)\n'
-            if condition[3] == 'percent':
-                buylistStr = buylistStr + '    df = df.head(int(len(df) * '+str(condition[4])+' / 100))\n'
-            elif condition[3] == 'num':
-                buylistStr = buylistStr + '    df = df.head('+str(condition[4])+')\n'
-        elif condition[0] == 'tec':
-            if condition[1] == 'ma':
-                buylistStr = buylistStr + '    stocklist'+str(i)+' = techFactor.ma(stocklist, \'close\', tradedate, '+str(condition[3])+', '+str(condition[4])+', '+str(condition[4])+')\n'
-
-        buylistStr = buylistStr + '    stocklist'+str(i)+' = df.code.tolist()\r\n'
-
-    buylistStr = buylistStr + '    # buy stocklist\n'
-    buylistStr = buylistStr + '    stocklist = '
-    for i in range(len(buycondition_list)):
-        buylistStr = buylistStr + 'stocklist'+str(i)
-        if i != len(buycondition_list)-1:
-            buylistStr = buylistStr + '+'
-
-    '''
-    # 生成卖出股票代码
-    selllistStr = ''
-    for i in range(len(sellcondition_list)):
-        condition = sellcondition_list[i]
-    '''
-
     template = env.get_template('mytemplate.html')
-    codestr = template.render(periodType='D', changePeriod='1', startDateStr='2016-1-1', endDateStr='2017-1-1', initMoney='1000000', buylistStr=buylistStr)
+    codestr = template.render(periodType='D', changePeriod='1', startDateStr='2007-1-1', endDateStr='2017-1-1', initMoney='1000000', \
+                              stockExpression='', baseVariableExpression=r'MA5 = MA(trade_closeprice,5)\nMA10 = MA(trade_closeprice, 10)', \
+                              conditionVariableExpression=r'gx = CROSS(MA5, MA10)\nasc5 = SORT(MA5, asc, 5)' ,buylist=buylist, selllist=selllist)
     print codestr
     f = open('my.py', 'w')
     f.write(codestr)
@@ -135,17 +103,17 @@ def generateCode():
     load = loader()
     m = load.load("my.py")
     print m
-    #c = m["getStockList"]
-    #print dir(m)
-    #func = c
+    c = m["getStockList"]
+    print dir(m)
+    func = c
     #c()
-    #print c.getStockList()
+    # print c.getStockList()
 
 if __name__ == '__main__':
     #shutil.copyfile('D:\workspace\TestStrategy\common\BaseTools.py', 'D:\workspace\TestStrategy\common\BaseTools1.py')
     #execfile('D:\workspace\TestStrategy\common\BaseTools1.py')
-    c.startDateStr='2016-1-1'
-    c.endDateStr='2016-2-1'
-    #run()
-    generateCode()
+    c.startDateStr='2010-1-1'
+    c.endDateStr='2017-1-1'
+    run()
+    #generateCode()
     #print my.getStockList()
